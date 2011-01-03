cc13b77c2791b755a044e51b10b5b57708d4ea2e
Justin Kirby <justin.kirby@trafficmp.com>
2011-01-03 18:47:47 -0500

    fixed up plotr

--- priv/plotr/plotr.py
+++ priv/plotr/plotr.py
@@ -244,7 +244,7 @@ def get_options(argv):
         print >>sys.stderr," for help use --help"
         return (1,{})
 
-    config = {"data":""}
+    config = {}
     for o,a in opts:
         if o in ("-h","--help"):
             print usage()
