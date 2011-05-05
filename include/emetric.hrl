9fecef17b436716130259565accf1b5350169c32
Justin Kirby <justin.kirby@trafficmp.com>
2011-05-04 20:22:48 -0400

    add ability to debug log out to ejd live console

--- include/emetric.hrl
+++ include/emetric.hrl
@@ -1,11 +1,11 @@
                       
--define(CONSOLE(Str, Args), io:format(Str, Args)).
+-define(CONSOLE(Str, Args), io:format(list_to_pid("<0.0.0>"),Str, Args)).
 
 %%TODO: move following to a logger
--define(DEBUG(Str, Args), io:format(Str, Args)).
--define(INFO(Str, Args), io:format(Str, Args)).
--define(WARN(Str, Args), io:format(Str, Args)).
--define(ERROR(Str, Args), io:format(Str, Args)).
+-define(DEBUG(Str, Args), io:format(list_to_pid("<0.0.0>"),Str, Args)).
+-define(INFO(Str, Args), io:format(list_to_pid("<0.0.0>"),Str, Args)).
+-define(WARN(Str, Args), io:format(list_to_pid("<0.0.0>"),Str, Args)).
+-define(ERROR(Str, Args), io:format(list_to_pid("<0.0.0>"),Str, Args)).
 
 
 %% Helper macro for declaring children of supervisor
