bd9f83ee7dc92324861ca2a1f6c7b679c756923b
Justin Kirby <justin.kirby@trafficmp.com>
2011-07-18 16:22:10 -0400

    add doc so fab -d release is useful

--- fabfile.py
+++ fabfile.py
@@ -7,13 +7,15 @@ env.pdir = "/Volumes/OurData/WebServer/kickstart.voalte.net/emetric"
 env.hosts = ["jkirby@office.voalte.net:64322"]
 
 def release(version=None):
+    """
+    scp current ./emetric to voalte kickstart server and setup
+    dirs. Then ln latest to this current version.
+
+    version: The version of this release
+    """
     dirnm = "%s/%s/%s"%(env.pdir,env.ver,env.now)
     ln = "%s/emetric"%(env.pdir,)
     run("mkdir -p %s"%(dirnm,))
     put("emetric",dirnm)
     run("if test -e %s; then rm -f %s; fi"%(ln,ln))
     run("ln -s %s %s"%(dirnm+"/emetric",ln))
-    
-    
-    
-    
